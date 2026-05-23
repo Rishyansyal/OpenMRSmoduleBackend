597b5083da47784c00d509c953456a3025bd6d22
Rishyan Syal <karanrishyan@gmail.com>
2026-05-23 02:39:25 +0200

    Verwijder hardcoded credentials uit code en config

--- Infrastructure/Messaging/Options/AsyncFlowOptions.cs
+++ Infrastructure/Messaging/Options/AsyncFlowOptions.cs
@@ -3,5 +3,5 @@ namespace Infrastructure.Messaging.Options;
 public class AsyncFlowOptions
 {
     public string BaseUrl { get; set; } = "http://localhost:1337";
-    public string ApiKey { get; set; } = "asyncflow-api-key";
+    public string ApiKey { get; set; } = "";
 }
--- Infrastructure/Messaging/Options/SecurePostOptions.cs
+++ Infrastructure/Messaging/Options/SecurePostOptions.cs
@@ -3,6 +3,6 @@ namespace Infrastructure.Messaging.Options;
 public class SecurePostOptions
 {
     public string BaseUrl { get; set; } = "http://localhost:1337";
-    public string ClientId { get; set; } = "securepost-client-id";
-    public string ClientSecret { get; set; } = "securepost-secret-key";
+    public string ClientId { get; set; } = "";
+    public string ClientSecret { get; set; } = "";
 }
